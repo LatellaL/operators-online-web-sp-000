0b0581869e5aabd5b29a3720cfc855b16cd5f9d3
Latella L <lt.latella@gmail.com>
2021-06-08 01:22:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,16 @@
 def unsafe?(speed)
  if speed > 60
    return true
-  else 
+else speed < 40
+   return true
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed (40, 60)
+	  return false
+	  
 end
 	
 
